9f4f5042da19f86db5fca11bb3a656f38daefb85
Paulina Prorok <43781246+PaulinaP19@users.noreply.github.com>
2023-08-31 20:01:39 +0200

    Combine results from experiment replicates

---
+++ merge_replicates.R
@@ -0,0 +1,30 @@
+#merge replicates
+#chose directory containing your data
+setwd("/Volumes/CCHL-User/Paulina/03_Tipin_Timeless/00000001_PLA/PLA_PCNA_Tim_publication/combined")
+library(ggplot2)
+library(stringr)
+boxplots  <- list.files(pattern = "txt")
+
+merged = data.frame()
+for (f in boxplots){
+  
+  data <- read.delim(f)
+  merged <- as.data.frame(rbind(merged, data))
+}
+  sufix <- str_extract(boxplots[1], "_(.*)")
+  fileN <- paste0("merge_",  sufix)
+  write.table(merged, file= fileN, row.names = FALSE, sep = "\t")
+  
+  filenm <- sub(".txt", ".pdf", fileN)
+  
+  pdf(file = filenm,   # file name
+      width = 6, # The width of the plot in inches
+      height = 6) # The height of the plot in inches
+ 
+  
+  level_orderX <- c("Sp_Ctrl", "Sp_HU", "Sp_APH", "NSp_Ctrl", "NSp_HU", "NSp_APH")
+  p <- ggplot(merged, aes(fill=name, y=Count, x=factor(name, level=level_orderX))) + geom_boxplot(outlier.shape = NA) + theme_bw() + scale_fill_discrete(breaks=level_orderX)  
+  # create the file!
+  print(p)
+  dev.off()
+  
